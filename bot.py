713b3ab5f0d55d9a5f1e7351a29a95d0a7d257ea
hhoang308 <huyhoang3082001@gmail.com>
2024-11-11 00:29:31 +0700

    more fixes

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 ACCOUNT_USER = os.getenv('ACCOUNT_USER')
 
 # Kết nối đến cơ sở dữ liệu SQLite
-conn = sqlite3.connect('submissions_1.db')
+conn = sqlite3.connect('submissions_3.db')
 cursor = conn.cursor()
 
 # Tạo bảng nếu chưa tồn tại
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS completed_problems (
     title TEXT UNIQUE,
     accepted_date TEXT,
     review_next TEXT,
-    review_lastest TEXT,
+    review_latest TEXT,
     review_times INTEGER
 )
 ''')
@@ -63,15 +63,17 @@ def get_problem_link(title_slug):
 def update_or_add_problem(submission):
     title = submission['title']
     time_stamp = datetime.fromtimestamp(int(submission['timestamp'])).strftime('%Y-%m-%d') 
-    print(f"update_or_add_problem() {title} {time_stamp}")
-    cursor.execute('SELECT review_lastest FROM completed_problems WHERE title = ?', (title,))
+    cursor.execute('SELECT review_latest FROM completed_problems WHERE title = ?', (title,))
     existing = cursor.fetchone()
-    # print(f"update_or_add_problem() {title} {time_stamp} {existing}")
     today = datetime.now().strftime('%Y-%m-%d')
     if existing:  #This problem exists -> consider it is a new problem learned today or a revise problem
-        review_lastest = existing[0]
-        print(f"update_or_add_problem() {title} {time_stamp} {existing}")
-        if review_lastest != today:
+        review_latest = existing[0]
+        print(f"{title} existed")
+        cursor.execute('SELECT accepted_date FROM completed_problems WHERE title = ?', (title,))
+        accepted_date_result = cursor.fetchone()[0]
+        if time_stamp < accepted_date_result: #Submissions from latest to oldest -> overlap -> update accepted_date
+            cursor.execute('UPDATE completed_problems SET accepted_date = ? WHERE title = ?', (time_stamp, title))
+        elif review_latest != today:
             cursor.execute('UPDATE completed_problems SET review_latest = ? WHERE title = ?', (today, title))
             cursor.execute('SELECT review_times FROM completed_problems WHERE title = ?', (title,))
             review_times_result = cursor.fetchone()
@@ -81,22 +83,24 @@ def update_or_add_problem(submission):
                 cursor.execute('SELECT review_next FROM completed_problems WHERE title = ?', (title,))
                 review_next_result = cursor.fetchone()
                 if review_next_result:
-                    # Calculate new `review_next` date based on `review_lastest`
-                    review_next = (datetime.strptime(review_lastest, '%Y-%m-%d') + timedelta(days=2 * (review_times + 1))).strftime('%Y-%m-%d')
+                    # Calculate new `review_next` date based on `review_latest`
+                    review_next = (datetime.strptime(review_latest, '%Y-%m-%d') + timedelta(days=2 * (review_times + 1))).strftime('%Y-%m-%d')
                     cursor.execute('UPDATE completed_problems SET review_next = ? WHERE title = ?', (review_next, title))
                     print(f"Updated problem: {title} | review_next set to: {review_next}")
                 else:
                     print("No review_next found for the given title.")
             else:
                 print("No review_times found for the given title.")
+        else:
+            print("This Submissions have just accepted today for the first time, do nothing")
     else:  # This problem doesn't exist -> create it with default value
-        print("Can't find ", title)
+        print(f"{title} doesn't exist")
         accepted_date = time_stamp
-        review_lastest = time_stamp
+        review_latest = time_stamp
         review_next = (datetime.strptime(time_stamp, '%Y-%m-%d') + timedelta(days=2)).strftime('%Y-%m-%d')
-        cursor.execute('INSERT INTO completed_problems (title, accepted_date, review_next, review_lastest, review_times) VALUES (?, ?, ?, ?, ?)', 
-                       (title, accepted_date, review_next, review_lastest, 0))
-        print(f"Inserted new problem: {title} | accepted_date: {accepted_date} | review_next: {review_next}")
+        cursor.execute('INSERT INTO completed_problems (title, accepted_date, review_next, review_latest, review_times) VALUES (?, ?, ?, ?, ?)', 
+                       (title, accepted_date, review_next, review_latest, 0))
+        print(f"Inserted a new problem: {title} | accepted_date: {accepted_date} | review_next: {review_next}")
     print("update_or_add_problem() done")
     conn.commit()
 
@@ -104,22 +108,24 @@ class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    async def setup_hook(self) -> None:
+    async def setup_hook(self) -> None: 
         # create the background task and run it in the background
         self.bg_task = self.loop.create_task(self.my_background_task())
 
-        async def on_ready(self):
-            print(f'Logged in as {self.user} (ID: {self.user.id})')
-            print('------')
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
     @tasks.loop(seconds=60)
     async def my_background_task(self):
         await self.wait_until_ready()
         channel = self.get_channel(CHANNEL_ID)  # channel ID goes here
+
         submissions = fetch_submissions()
-        print("fetch_submissions() ", submissions)
-    
+        print("Fetched submissions:", submissions)
+
         today = datetime.now().strftime('%Y-%m-%d')
-        print("Today:", today)
+        print("Today's date:", today)
+
         new_problems = []
         to_review_problems = []
         reviewed_problems = []
@@ -128,27 +134,26 @@ class MyClient(discord.Client):
             accepted_date = datetime.fromtimestamp(int(submission['timestamp'])).strftime('%Y-%m-%d')
             print(f"Submission: {submission['title']} | Accepted Date: {accepted_date}")            
             if accepted_date >= '2024-11-07':  # Lọc kết quả từ 28/10/2024 trở về hiện tại
-                print("Valid submission because this submission was accepted after ", accepted_date)
+                print("Valid Submission")
                 update_or_add_problem(submission)
             else:
-                print("Invalid submission because this submission was accepted before ", accepted_date)
+                print("Invalid Submission")
 
-        cursor.execute('SELECT title, accepted_date, review_next , review_lastest FROM completed_problems')
+        cursor.execute('SELECT title, accepted_date, review_next , review_latest FROM completed_problems')
         all_problems = cursor.fetchall()
 
-        for title, accepted_date, review_next, review_lastest in all_problems:
+        for title, accepted_date, review_next, review_latest in all_problems:
             if accepted_date == today:
                 new_problems.append(f"{title}: {get_problem_link(convert_to_slug(title))}")
-            if review_next <= today and review_lastest != today:
+            if review_next <= today and review_latest != today:
                 to_review_problems.append(f"{title}: {get_problem_link(convert_to_slug(title))}")
-            if review_lastest == today and accepted_date != today:
+            if review_latest == today and accepted_date != today:
                 reviewed_problems.append(f"{title}: {get_problem_link(convert_to_slug(title))}")
 
         # Tạo thông báo
         message = f"Hôm nay bạn đã làm {len(new_problems)} bài mới:\n" + "\n".join(new_problems) + "\n\n"
         message += f"Hôm nay cần ôn luyện các bài sau:\n" + "\n".join(to_review_problems) + "\n\n"
         message += f"Những bài đã ôn luyện hôm nay:\n" + "\n".join(reviewed_problems)
-        conn.close()
 
         await channel.send(message)
 
